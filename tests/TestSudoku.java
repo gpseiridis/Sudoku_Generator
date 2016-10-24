58790d5dfc7e6abe2ec05b0135abb890a12c6c06
GomUser1 <g.pseiridis@gmail.com>
2016-10-25 01:16:40 +0200

    added another test case

--- tests/TestSudoku.java
+++ tests/TestSudoku.java
@@ -10,12 +10,11 @@ SudokuGen mSudoku = new SudokuGen();
 	// test 2 : columns = 45
 	// test 3 : tetragwno... TO PIO DUSKOLO
 	// test 4 : des mhpws yparxei arithmos panw apo 9 'h katw apo 1
-	//           tha borouse px na nai 11, -2 values
+	//          tha borouse px na nai 11, -2 values
 	// test 5: check if length is 9
 	
-	
+int expectedSum = 45;	
 	@Test
-
 	public void testRowLength() {
 		int expectedLength[] = { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
 		int actualRowLength[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -26,40 +25,64 @@ SudokuGen mSudoku = new SudokuGen();
 		}
 	}
 		
+	
 	@Test
 	public void testRows(){
-
+		
 		int mockArray[]= {1,2,3,4,4,6,7,8,9}; //<--fails, actualSum = 45	
 		int mockArray2[]= {12,4,3,4,4,6,-7,10,9}; // <-- passes the test of actual sum = 45
-		int expectedSum = 45;
-		int actualSum = 0;
+		int actualRowSum = 0;
 
 		mSudoku.genFirstRow();
 		
 
-		
-
 		//unique numbers will give a sum(1 to 9) = 45
 		for(int row=0; row<9; row++){
-			
-
-
 			for(int i=0; i<9; i++){				
 
 				
-				actualSum += mSudoku.sudokuArray[1][i]; 
+				actualRowSum += mSudoku.sudokuArray[row][i]; 
 				//actualSum += mockArray2[i];
 
 			}
 			System.out.println();
-			System.out.println("for row: " +(row+1)+ " actual sum is: " +actualSum);
+			System.out.println("for row: " +(row+1)+ " actual sum is: " +actualRowSum);
 
-			assertEquals(expectedSum,actualSum);
+			assertEquals(expectedSum,actualRowSum);
 
-			actualSum=0;
+			actualRowSum=0;
+		
 		}//end ROW for
-
+		
 	}//end testing rows
+		
+	
+		@Test
+		public void testCollumns(){
+			mSudoku.genFirstRow();
+			int actualColumnSum =0;
+			for(int column=0; column<9; column++){
+				
+				for(int i=0; i<9; i++){		
+					
+				
+				actualColumnSum += mSudoku.sudokuArray[i][column]; 
+				
+				
+				}
+				System.out.println();
+				System.out.println("for column: " +(column+1)+ " actual sum is: " +actualColumnSum);
+				assertEquals(expectedSum,actualColumnSum);
+				actualColumnSum = 0; 
+			}//end for column
+			
+		
+		
+		}//end testing collumns
+		
+		
+}//end of class
+
+	
 	
 
-}
