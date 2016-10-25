318ae3db42d4772c7d5b46e06f7ff725c3608e0a
GomUser1 <g.pseiridis@gmail.com>
2016-10-25 22:57:25 +0200

    added a test case for making sure no number is less than 1 or bigger than 9

--- tests/TestSudoku.java
+++ tests/TestSudoku.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.IsNot;
 import org.junit.Test;
 
 public class TestSudoku {
@@ -16,14 +17,18 @@ SudokuGen mSudoku = new SudokuGen();
 	
 int expectedSum = 45;	
 int limit = mSudoku.sudokuArray.length;
-public int testArray[][];
-	
+int mockArray[]= {1,2,3,4,4,6,7,8,9}; //<--fails, actualSum = 45	
+int mockArray2[]= {12,4,3,4,4,6,-7,10,9}; // <-- passes the test of actual sum = 45
+
+
+
+
 
 	
-	//testing row length, init the actualRowLength and test it later
+	//test1:  each row should not have length bigger than 9
 	@Test
-	public void testRowLength() {
-
+	public void testRowLength() {	
+		mSudoku.genFirstRow();
 		int expectedLength[] = { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
 		int actualRowLength[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 		for (int row = 0; row < limit; row++) {
@@ -32,25 +37,49 @@ public int testArray[][];
 
 		}
 	}
+	
+	//test2: no value should be bigger than 9 or less than 1
+	@Test
+	public void testInvalidValues() {	
+		mSudoku.genFirstRow();
+		boolean isValidValue = false;
 		
+		for (int row = 0; row < limit; row++) {
+			for (int column = 0; column < limit; column++) {
+				//testing this test case using mock arrays
+				//if(mockArray2[row]<=9 && mockArray2[row]>0){ <-- will fail, since there are bigger than 9 and negatives
+				//if(mockArray[row]<=9 && mockArray[row]>0){  <-- passes, since there is no invalid number
+					
+				if(mSudoku.sudokuArray[row][column]<=9 && mSudoku.sudokuArray[row][column]>0){
+					isValidValue= true;
+				}else{
+					isValidValue= false;
+				}
+				assertTrue(isValidValue);
+			}
+
+		}
+		
+	}
+
+
 	//unique numbers will give a sum(1 to 9) = 45
-	//testing if all rows will have a sum of 45
+	//test 3: sum of all rows should be equal to 45
 	@Test
 	public void testRows(){
 		
-		int mockArray[]= {1,2,3,4,4,6,7,8,9}; //<--fails, actualSum = 45	
-		int mockArray2[]= {12,4,3,4,4,6,-7,10,9}; // <-- passes the test of actual sum = 45
+		
 		int actualRowSum = 0;
 
 		mSudoku.genFirstRow();
 			
 		for(int row=0; row<limit; row++){
-			for(int i=0; i<9; i++){				
+			for(int column=0; column<9; column++){				
 
 				
-			actualRowSum += mSudoku.sudokuArray[row][i]; 
+			actualRowSum += mSudoku.sudokuArray[row][column]; 
 				
-				//actualSum += mockArray2[i];
+				//actualSum += mockArray2[i];    <--- this will pass
 
 			}
 			System.out.println();
@@ -65,51 +94,51 @@ public int testArray[][];
 	}//end testing rows
 		
 	
-		//testing if all collumns have sume of 45
-		@Test
-		public void testCollumns(){
-			mSudoku.genFirstRow();
-			int actualColumnSum =0;
-			for(int column=0; column<limit; column++){
-				
-				for(int i=0; i<limit; i++){		
-					
-				
-				actualColumnSum += mSudoku.sudokuArray[i][column]; 
-				
-				
-				}
-				System.out.println();
-				System.out.println("for column: " +(column+1)+ " actual sum is: " +actualColumnSum);
-				assertEquals(expectedSum,actualColumnSum);
-				actualColumnSum = 0; 
-			}//end for column
-			
-		
-		
-		}//end testing collumns
-		
-		//testing if all the boxes have a sum of 45
-		@Test
-		public void testBoxes(){
-			mSudoku.genFirstRow();
-			int actualBoxSum=0;
+	//test 4: sum of all columns should be equal to 45
+	@Test
+	public void testCollumns(){
+		mSudoku.genFirstRow();
+		int actualColumnSum =0;
+		for(int column=0; column<limit; column++){
+
+			for(int row=0; row<limit; row++){		
+
+
+				actualColumnSum += mSudoku.sudokuArray[row][column]; 
+
+
+			}
 			System.out.println();
-			
-			//basically each box gets unfolded to a row. Each row is then compared to expected sum = 45
-			for(int i=0; i<limit; i+=3){
-				System.out.println();
-				for(int j=0; j<limit; j+=3){
-					System.out.println(mSudoku.sudokuArray[i][j]);
-					actualBoxSum+= mSudoku.sudokuArray[i][j];
-					
-				}
-			}			
-			assertEquals(expectedSum,actualBoxSum);
-			actualBoxSum = 0; 
-			
-		}
-		
+			System.out.println("for column: " +(column+1)+ " actual sum is: " +actualColumnSum);
+			assertEquals(expectedSum,actualColumnSum);
+			actualColumnSum = 0; 
+		}//end for column
+
+
+
+	}//end testing collumns
+
+	//test 5: sum of all boxes should be equal to 45 
+	@Test
+	public void testBoxes(){
+		mSudoku.genFirstRow();
+		int actualBoxSum=0;
+		System.out.println();
+
+		//basically each box gets unfolded to a row. Each row is then compared to expected sum = 45
+		for(int row=0; row<limit; row+=3){
+			System.out.println();
+			for(int column=0; column<limit; column+=3){
+				System.out.println(mSudoku.sudokuArray[row][column]);
+				actualBoxSum+= mSudoku.sudokuArray[row][column];
+
+			}
+		}			
+		assertEquals(expectedSum,actualBoxSum);
+		actualBoxSum = 0; 
+
+	}
+
 }//end of class
 
 	
